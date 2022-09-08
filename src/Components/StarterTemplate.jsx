61337dc40f615f6f2ccc3a0b9b8fabea4c709e56
NAGATEJA V <95696533+teja2004@users.noreply.github.com>
2022-09-08 21:11:33 +0530

    eDAM 21:11 08-09-2022 Some Changes in Footer and Depts..

--- src/Components/StarterTemplate.jsx
+++ src/Components/StarterTemplate.jsx
@@ -14,9 +14,10 @@ export const StarterTemplate = () => {
   return (
     <>
         <section className="bg-violet-200 w-[100%] h-auto flex flex-col lg:flex-row items-center p-5">
-          <div className="order-2 lg:order-1 flex flex-col w-[60%] lg:ml-5 pb-10">
-            <h1 className="text-3xl mb-3 hover:underline break-words">e - Designing and Marketing</h1>
-            <h2 className="text-md my-1 hover:text-violet-900 break-words">e - DAM is a student-based club of IARE that aims to build a technically well-equipped group with the most resourceful digital skills required in the modern workplace, to create and develop websites, applications and designing.</h2>
+          <div className="order-2 lg:order-1 flex flex-col w-[98%] lg:w-[60%] lg:ml-5 pb-5">
+            <h1 className="md:text-3xl mb-3 md:hover:underline break-words text-xl">e - DAM</h1>
+            <div className="line my-1 md:my-2 w-[12%] h-[0.10rem] md:w-[8%] md:h-1 rounded-sm bg-violet-700"></div>
+            <h2 className="text-md my-1 hover:text-violet-900 break-words w-[90%] lg:w-auto">e - DAM is a student-based club of IARE that aims to build a technically well-equipped group with the most resourceful digital skills required in the modern workplace, to create and develop websites, applications and designing.</h2>
             <Link className="bg-violet-500 max-w-fit w-[100%] p-2 my-2 text-white rounded-md hover:bg-violet-900 break-words" to="/departments">Depatments</Link>
           </div>
           <div className="order-1 lg:order-2 w-[40%]">
@@ -24,9 +25,9 @@ export const StarterTemplate = () => {
           </div>
         </section>
         <section className="bg-blue-100 w-[100%] h-auto flex flex-col items-center justify-center py-10">
-          <article className="flex flex-col items-center w-[100%] max-w-[50%] my-2">
+          <article className="flex flex-col items-start md:items-center w-[100%] max-w-[90%] md:max-w-[60%] my-1 lg:my-2 justify-center">
             <h1 className="my-2 text-3xl text-blue-800 hover:underline text-center">About Us</h1>
-            <h2 className="my-2 text-center break-words">e - Designing and Marketing (e - DAM) is a student club of IARE, which focuses on developing the develope culuture among students in the college and work on college-projects, or work as intern's in companies {seeMore && <button id="moreBtn" className="text-blue-900 hover:underline px-1 py-[0.10rem] hover:text-black font-semibold" onClick={()=>{
+            <h2 className="my-2 text-left text-md lg:text-lg  lg:text-center break-words w-[100%]">e - Designing and Marketing (e - DAM) is a student club of IARE, which focuses on developing the develope culuture among students in the college and work on college-projects, or work as intern's in companies {seeMore && <button id="moreBtn" className="text-blue-900 hover:underline px-1 py-[0.10rem] hover:text-black font-semibold" onClick={()=>{
               setMore(true);
               setSeeMore(false);
               setSeeLess(true);
